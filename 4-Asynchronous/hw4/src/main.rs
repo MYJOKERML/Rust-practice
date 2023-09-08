f84ce7ec2a02850e5973f0d62460d0ad998aa863
MYJOKERML <280215476@qq.com>
2023-09-08 11:29:08 +0800

    update hw4

--- 4-Asynchronous/hw4/src/main.rs
+++ 4-Asynchronous/hw4/src/main.rs
@@ -1,71 +1,38 @@
-use tokio::sync::mpsc;
-use std::sync::{Arc, Mutex};
-
-struct Runtime {
-    tx: mpsc::Sender<Box<dyn FnOnce() + Send>>,
+use std::{
+    future::Future,
+    sync::{Arc, Condvar, Mutex},
+    task::{Context, Poll, Wake, Waker},
 }
 
-impl Runtime {
+#[cfg(feature = "macro")]
+pub use pollster_macro::{main, test};
+
+pub trait FutureExt: Future {
+    fn block_on(self) -> Self::Output where Self: Sized { block_on(self) }
+}
+
+impl<F: Future> FutureExt for F {}
+
+enum SignalState {
+    Empty,
+    Waiting,
+    Notified,
+}
+
+struct Signal {
+    state: Mutex<SignalState>,
+    cond: Condvar,
+}
+
+impl Signal {
     fn new() -> Self {
-        let (tx, rx) = mpsc::channel::<Box<dyn FnOnce() + Send>>(100);
-        let rt = Arc::new(Mutex::new(Runtime {
-            tx,
-        }));
-
-        tokio::spawn(async move {
-            while let Some(task) = rx.recv().await {
-                task();
-            }
-        });
-
-        Runtime {
-            tx: tx.clone(),
+        Self {
+            state: Mutex::new(SignalState::Empty),
+            cond: Condvar::new(),
         }
     }
 
-    fn spawn<F>(&self, future: F)
-    where
-        F: std::future::Future<Output = ()> + Send + 'static,
-    {
-        let tx = self.tx.clone();
-        let task = Box::new(move || {
-            tokio::spawn(async move {
-                future.await;
-            });
-        });
-
-        tx.blocking_send(task).unwrap();
+    fn wait(&self) {
+        let mut state = self.state.lock().unwrap();
     }
-
-    fn block_on<F>(&self, future: F)
-    where
-        F: std::future::Future<Output = ()>,
-    {
-        let mut rt = self.tx.lock().unwrap();
-        let mut local = tokio::runtime::Builder::new()
-            .basic_scheduler()
-            .enable_all()
-            .build()
-            .unwrap();
-        
-        local.block_on(future);
-    }
-}
-
-#[tokio::main]
-async fn main() {
-    let runtime = Arc::new(Runtime::new());
-
-    for i in 0..10 {
-        let runtime_clone = runtime.clone();
-        runtime.spawn(async move {
-            println!("Task {} is running.", i);
-            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-            println!("Task {} is done.", i);
-        });
-    }
-
-    runtime.block_on(async {
-        println!("All tasks are completed.");
-    });
 }
